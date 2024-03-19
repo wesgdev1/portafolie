ed3bbd3308e04e8b448bda06655de7eec61de85c
Welinton Suarez <wesgdeveloper@gmail.com>
2024-03-19 17:34:38 -0500

    update projects information

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,8 +4,9 @@ interface Props {
   title: string;
   link: string;
   image: string;
+  status: boolean;
 }
-export const Project = ({ title, link, image }: Props) => {
+export const Project = ({ title, link, image, status }: Props) => {
   return (
     <a href={link} target="_blank">
       <div className="w-full h-80 border-[1px] border-blue-600 overflow-hidden rounded-lg group relative hover:shadow-red-500 ">
@@ -16,9 +17,15 @@ export const Project = ({ title, link, image }: Props) => {
           width={355}
           height={355}
         />
-        <p className="absolute bottom-0 w-full py-1 bg-blue-600 text-white text-center hidden group-hover:inline-block duration-300">
-          {title}
-        </p>
+        {status ? (
+          <p className="absolute bottom-0 w-full py-1  bg-blue-600 text-white text-center hidden group-hover:inline-block duration-300">
+            {title}
+          </p>
+        ) : (
+          <p className="absolute bottom-0 w-full py-1  bg-red-600 text-white text-center hidden group-hover:inline-block duration-300">
+            {title}
+          </p>
+        )}
       </div>
     </a>
   );
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,10 @@ import { GoProjectSymlink } from "react-icons/go";
 import { Title } from "./Title";
 
 import amazonClone from "../public/img/projects/amazonClone.webp";
+import theGarage from "../public/img/projects/theGarage.png";
+import prolinkApp from "../public/img/projects/prolinkApp.png";
+import amvKids from "../public/img/projects/amvKids.png";
+import petStay from "../public/img/projects/petStay.png";
 import { Project } from "./Project";
 
 export const Projects = () => {
@@ -10,29 +14,28 @@ export const Projects = () => {
       <Title text="Proyectos " icon={<GoProjectSymlink />} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
         <Project
-          title="prueba"
+          title="The Garage App"
           link={"https://the-garage-app.netlify.app/home"}
-          image={amazonClone.src}
+          image={theGarage.src}
+          status={true}
         />
         <Project
-          title="prueba"
-          link={"https://the-garage-app.netlify.app/home"}
-          image={amazonClone.src}
+          title="Prolink App"
+          link={"https://prolink-ncf0.onrender.com/home"}
+          image={prolinkApp.src}
+          status={true}
         />
         <Project
-          title="prueba"
-          link={"https://www.google.com"}
-          image={amazonClone.src}
+          title="Amv Kids - En progreso.."
+          link={"https://amvkids.com.co/"}
+          image={amvKids.src}
+          status={false}
         />
         <Project
-          title="prueba"
-          link={"https://the-garage-app.netlify.app/home"}
-          image={amazonClone.src}
-        />
-        <Project
-          title="prueba"
-          link={"https://the-garage-app.netlify.app/home"}
-          image={amazonClone.src}
+          title="Pet Stay"
+          link={"https://petstay.onrender.com/"}
+          image={petStay.src}
+          status={true}
         />
       </div>
     </div>
