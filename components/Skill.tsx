f082927d2e6e39cf3a59ccef8a527c6f1e499920
Welinton Suarez <wesgdeveloper@gmail.com>
2024-03-19 17:42:15 -0500

    update icons for skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,26 +1,35 @@
 import { Skillinput } from "./Skillinput";
 import { Title } from "./Title";
 import { GiSkills } from "react-icons/gi";
-import { IoLogoJavascript } from "react-icons/io";
+import { SiPrisma } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { FaReact } from "react-icons/fa";
+import { FaBootstrap } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+import { TbBrandTailwind } from "react-icons/tb";
+import { FaGithub } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
 import { RiJavascriptLine } from "react-icons/ri";
+import { FaNode } from "react-icons/fa";
+import { SiNestjs } from "react-icons/si";
 
 export const Skill = () => {
   return (
     <div className="wrapper">
       <Title text="Skills" icon={<GiSkills />} />
       <div className="flex gap-5 flex-wrap justify-center">
-        <Skillinput title="Node" icon={<RiJavascriptLine />} />
+        <Skillinput title="Node" icon={<FaNode />} />
         <Skillinput title="Javascript" icon={<RiJavascriptLine />} />
         <Skillinput title="Express" icon={<RiJavascriptLine />} />
-        <Skillinput title="Prisma" icon={<RiJavascriptLine />} />
-        <Skillinput title="Postgresql" icon={<RiJavascriptLine />} />
-        <Skillinput title="React" icon={<RiJavascriptLine />} />
-        <Skillinput title="Redux toolkit" icon={<RiJavascriptLine />} />
-        <Skillinput title="Boostrap" icon={<RiJavascriptLine />} />
-        <Skillinput title="Tailwind" icon={<RiJavascriptLine />} />
-        <Skillinput title="Git/ Github" icon={<RiJavascriptLine />} />
-        <Skillinput title="NextJs" icon={<RiJavascriptLine />} />
-        <Skillinput title="NestJs" icon={<RiJavascriptLine />} />
+        <Skillinput title="Prisma" icon={<SiPrisma />} />
+        <Skillinput title="Postgresql" icon={<BiLogoPostgresql />} />
+        <Skillinput title="React" icon={<FaReact />} />
+        <Skillinput title="Redux toolkit" icon={<SiRedux />} />
+        <Skillinput title="Boostrap" icon={<FaBootstrap />} />
+        <Skillinput title="Tailwind" icon={<TbBrandTailwind />} />
+        <Skillinput title="Git/ Github" icon={<FaGithub />} />
+        <Skillinput title="NextJs" icon={<TbBrandNextjs />} />
+        <Skillinput title="NestJs" icon={<SiNestjs />} />
       </div>
     </div>
   );
