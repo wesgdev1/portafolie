9bb5cb1ecfd1707794a2734b420d2f96d7975245
Welinton Suarez <wesgdeveloper@gmail.com>
2024-03-19 19:20:09 -0500

    update: description

--- components/About.tsx
+++ components/About.tsx
@@ -7,19 +7,12 @@ const About = () => {
       <Title text="Acerca de mi" icon={<SiInformatica />} />
       <div className="text-lg tracking-wide flex flex-col gap-5 text-justify">
         <p>
-          Desarrollador Full-Stack apasionado por el desarrollo web y las
-          telecomunicaciones. Especializado en JavaScript, Node, React, Express,
-          SQL, y Git. Mi pasión por el desarrollo de software y las
-          telecomunicaciones se fusionan en un conjunto de habilidades
-          diversificadas que me permiten abordar desafíos con creatividad y
-          enfoque. Más allá de mi dedicación al ámbito tecnológico, encontrarás
-          en mí un entusiasta y amante del deporte. Considero que mantener un
-          equilibrio entre el mundo digital y el bienestar físico es esencial
-          para el crecimiento integral. Me gustaría conectar con colegas de la
-          industria y embarcarnos juntos en proyectos estimulantes. Si buscas
-          oportunidades en el campo del desarrollo web, la tecnología y las
-          telecomunicaciones, o simplemente deseas intercambiar ideas, estaré
-          encantado de estar en contacto.
+          Soy un apasionado Desarrollador Full-Stack especializado en
+          JavaScript, Node, React, y SQL. Mi fascinación por el desarrollo web
+          se une a mi interés en las telecomunicaciones, creando un combo de
+          habilidades que abordan desafíos con creatividad. Amante del deporte,
+          creo en el equilibrio entre lo digital y el bienestar físico.
+          ¡Conéctemos y sumerjámonos en proyectos emocionantes juntos!
         </p>
       </div>
     </div>
