4e1e5a2bbf8653c5bce0e8cc0f1951e3ef732d5c
Welinton Suarez <wesgdeveloper@gmail.com>
2024-04-30 11:14:32 -0500

    Add new project and update existing projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,32 +1,55 @@
 import Image from "next/image";
 
 interface Props {
+  role: string;
   title: string;
   link: string;
   image: string;
   status: boolean;
+  linkRepo?: string;
 }
-export const Project = ({ title, link, image, status }: Props) => {
+export const Project = ({
+  linkRepo,
+  role,
+  title,
+  link,
+  image,
+  status,
+}: Props) => {
   return (
-    <a href={link} target="_blank">
-      <div className="w-full h-80 border-[1px] border-blue-600 overflow-hidden rounded-lg group relative hover:shadow-red-500 ">
-        <Image
-          className=" object-cover  group-hover:translate-y-[-70%] transition-transform duration-[4s]"
-          src={image}
-          alt="Project"
-          width={355}
-          height={355}
-        />
-        {status ? (
-          <p className="absolute bottom-0 w-full py-1  bg-blue-600 text-white text-center hidden group-hover:inline-block duration-300">
-            {title}
-          </p>
-        ) : (
-          <p className="absolute bottom-0 w-full py-1  bg-red-600 text-white text-center hidden group-hover:inline-block duration-300">
-            {title}
-          </p>
+    <div className="flex flex-col">
+      <div className="mx-2 my-4  px-3 border-l-4 border-blue-600   ">
+        <h3>Rol: {role}</h3>
+        {linkRepo && (
+          <a
+            href={linkRepo}
+            target="_blank"
+            className="font-bold text-blue-400 hover:text-gray-600"
+          >
+            <p>Ver repositorio</p>
+          </a>
         )}
       </div>
-    </a>
+      <a href={link} target="_blank">
+        <div className="w-full h-80 border-[1px] border-blue-600 overflow-hidden rounded-lg group relative hover:shadow-red-500 ">
+          <Image
+            className=" object-cover  group-hover:translate-y-[-70%] transition-transform duration-[4s]"
+            src={image}
+            alt="Project"
+            width={355}
+            height={355}
+          />
+          {status ? (
+            <p className="absolute bottom-0 w-full py-1  bg-blue-600 text-white text-center hidden group-hover:inline-block duration-300">
+              {title}
+            </p>
+          ) : (
+            <p className="absolute bottom-0 w-full py-1  bg-red-600 text-white text-center hidden group-hover:inline-block duration-300">
+              {title}
+            </p>
+          )}
+        </div>
+      </a>
+    </div>
   );
 };
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,6 +6,7 @@ import theGarage from "../public/img/projects/theGarage.png";
 import prolinkApp from "../public/img/projects/prolinkApp.png";
 import amvKids from "../public/img/projects/amvKids.png";
 import petStay from "../public/img/projects/petStay.png";
+import noa from "../public/img/projects/noa.png";
 import { Project } from "./Project";
 
 export const Projects = () => {
@@ -14,28 +15,44 @@ export const Projects = () => {
       <Title text="Proyectos " icon={<GoProjectSymlink />} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
         <Project
+          role="Desarrollador FullStack"
           title="The Garage App"
           link={"https://the-garage-app.netlify.app/home"}
           image={theGarage.src}
           status={true}
+          linkRepo="https://github.com/ManuelOrtegaR/the-garage"
         />
         <Project
+          role="Desarrollador FullStack"
           title="Prolink App"
           link={"https://prolink-ncf0.onrender.com/home"}
           image={prolinkApp.src}
           status={true}
+          linkRepo="https://github.com/wesgdev1/prolink-api"
         />
         <Project
+          role="Desarrollador Backend"
+          title="Noa"
+          link={"https://noalone.vercel.app/"}
+          image={noa.src}
+          status={true}
+          linkRepo={"https://github.com/No-Country/s14-09-ft-node-react"}
+        />
+        <Project
+          role="Desarrollador FullStack"
           title="Amv Kids - En progreso.."
           link={"https://amvkids.com.co/"}
           image={amvKids.src}
           status={false}
+          linkRepo="https://github.com/wesgdev1/amvKids"
         />
         <Project
+          role="Desarrollador Backend"
           title="Pet Stay"
           link={"https://petstay.onrender.com/"}
           image={petStay.src}
           status={true}
+          linkRepo={"https://github.com/No-Country/c16-16-t-node-react"}
         />
       </div>
     </div>
